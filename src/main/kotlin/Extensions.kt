4fd8667b2f0730492f49e2a98081b2973e28cff5
JoJaCo <contrerasjorge226@gmail.com>
2025-03-11 01:18:17 -0700

    continue to work on extension with Generuc

---
+++ src/main/kotlin/Extensions.kt
@@ -0,0 +1,14 @@
+
+fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+
+fun <T> T.easyPrint(): T {
+
+    println(this)
+    return this
+
+}
+
+fun main(args: Array<String>){
+    "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
+    42.easyPrint()
+}
