8428710f77d518c37d73c560b4aaa7b43086d4f1
JoJaCo <contrerasjorge226@gmail.com>
2025-03-11 04:42:27 -0700

    continue to work on extension with Extension Properties

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -8,13 +8,17 @@ fun <T> T.easyPrint(): T {
 
     println(this)
     return this
-
 }
+
+infix fun String?.printWithDefault(default: String) = print(this ?: default)
 
 fun main(args: Array<String>){
 
     "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
     "How many vowels?".numVowels.easyPrint()
+
+    val nullableString: String? = null
+    nullableString printWithDefault "Default string"
 
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import java.util.logging.Handler
+
+fun main(args: Array<String>) {
+
+
+    val name = 'u'
+    var newLetter = name.uppercaseChar()
+
+        if(newLetter == 'U'){
+            println("yes")
+
+            }else{
+                print("no")
+            }
+
+
+}
+
