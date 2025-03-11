6b4ca1f1bd65c214c5a35df645c803e0b6539dc1
JoJaCo <contrerasjorge226@gmail.com>
2025-03-11 03:56:06 -0700

    continue to work on extension with Extension Properties

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,4 +1,7 @@
 
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
+
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
 fun <T> T.easyPrint(): T {
@@ -9,6 +12,9 @@ fun <T> T.easyPrint(): T {
 }
 
 fun main(args: Array<String>){
+
     "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
+
 }
